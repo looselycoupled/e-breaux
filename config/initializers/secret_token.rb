0e21afb9a62b9908fed16665526bb0795f72e80c
Allen Leis <allen.leis@threespot.com>
2011-07-25 14:46:09 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EBreaux::Application.config.secret_token = '9fe59de3a88b0acb0aae2863342e98f375f2826fe0847e3dd723edd1504ac06d24000fd4eebc5fdfacaeaad4403ba3a9fc459c06ef79c65cbb6314009a5c707b'
