3f4022d2416afc0dd8e7f0f2b661a2da3c161b64
Allen Leis <allen.leis@threespot.com>
2011-08-06 15:09:28 -0400

    periodic commit; working code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,32 @@ class DashboardController < ApplicationController
   layout "authenticated"
 
   def index
+    # @posts = Egeaux::Ebreaux.scan(current_user.access_token)
+    # access_token = current_user.access_token
+
+    # access_token = Facebook.last.access_token
+    # 
+    # 
+    # # code belongs in lib
+    # fb = FbGraph::User.me(access_token).fetch
+    # @posts = fb.posts
+    
+  end
+  
+  # scan for new posts.  schedule responses
+  def scan_new_posts
+    # *** scan for new posts ***
+    subscriptions = Subscription.all
+    subscriptions.each {|s|
+      
+    }
+    
+    # *** schedule responses ***
+    
+  end
+  
+  # 
+  def post
     
   end
 
