71b26eeb937b2bda9e3ce4954fce3b27c06a8dd6
Allen Leis <allen.leis@threespot.com>
2011-08-06 15:06:04 -0400

    adds posts method to get target posts

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,10 @@
 class Subscription < ActiveRecord::Base
   belongs_to :facebook
   belongs_to :target
+  
+  def posts
+    access_token = facebook.access_token
+    FbGraph::User.fetch(target.identifier,:access_token => facebook.access_token).posts
+  end
 
 end
