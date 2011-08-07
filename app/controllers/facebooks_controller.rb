6c5809737241512c329a792bfe98c117c5c616b2
Allen Leis <allen.leis@threespot.com>
2011-08-06 22:12:46 -0400

    removes impersonate code

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -30,38 +30,6 @@ class FacebooksController < ApplicationController
   end
 
 
-  def impersonate
-    session[:current_user] = params[:id]
-    redirect_to root_url
-  end
-  # 
-  # # handle Normal OAuth flow: start
-  # def new
-  #   redirect_to client.authorization_uri(
-  #     :scope => Facebook.config[:perms]
-  #   )
-  # end
-  # 
-  # # handle Normal OAuth flow: callback
-  # def create
-  #   # save code and access_token
-  #   client.authorization_code = params[:code]
-  #   access_token = client.access_token!
-  #   
-  #   # fetch user details (identifier)
-  #   user = FbGraph::User.me(access_token).fetch
-  # 
-  #   # create facebook record if needed; save session/cookie data
-  #   Facebook.identify(user)
-  #   
-  #   # redirect to confirmation_url
-  #   redirect_to :action => :confirm
-  # end
-  # 
-  # def confirm
-  #   
-  # end
-
   private
 
   def client
