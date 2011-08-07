a8d77c76cf858aab3353ab7d9b0a30a3b6455664
Allen Leis <allen.leis@threespot.com>
2011-08-06 20:50:41 -0400

    adds active field to response

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110806215347) do
+ActiveRecord::Schema.define(:version => 20110807004212) do
 
   create_table "comments", :force => true do |t|
     t.string   "message"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20110806215347) do
     t.datetime "respond_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active",      :default => true
   end
 
   create_table "subscriptions", :force => true do |t|
