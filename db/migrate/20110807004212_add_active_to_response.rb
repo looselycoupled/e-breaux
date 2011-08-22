a247ee9b46b40a3e6577f6ebb0884916912d7609
Allen Leis <allen.leis@threespot.com>
2011-08-22 15:16:54 -0400

    changes default boolean value to true for PG

--- db/migrate/20110807004212_add_active_to_response.rb
+++ db/migrate/20110807004212_add_active_to_response.rb
@@ -1,6 +1,6 @@
 class AddActiveToResponse < ActiveRecord::Migration
   def self.up
-    add_column :responses, :active, :boolean, :default => 1
+    add_column :responses, :active, :boolean, :default => true
   end
 
   def self.down
